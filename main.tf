47153dbc6a09023d3fe08a50105caa1681d6ccea
flx5 <1330854+flx5@users.noreply.github.com>
2022-03-05 19:50:09 +0100

    Forward flatcar ip to docker

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+output "flatcar_ip" {
+  # TODO What about other (>= 1) values?
+  value = values(module.flatcar.ip-addresses)[0][0]
+}
+
+
 module "flatcar" {
    source = "./modules/flatcar-ref"
    base_image = "${var.base_image}"
@@ -5,10 +11,9 @@ module "flatcar" {
    machines = "${var.machines}"
    virtual_memory = "${var.virtual_memory}"
    ssh_keys = "${var.ssh_keys}"
-   
-   # TODO Figure out where output from module lands
 }
 
 module "flatcar-docker" {
    source = "./modules/flatcar-containers"
+   docker_ssh = values(module.flatcar.ip-addresses)[0][0]
 }
---
+++ modules/flatcar-containers/variables.tf
@@ -0,0 +1,4 @@
+variable "docker_ssh" {
+  type        = string
+  description = "Docker SSH"
+}
