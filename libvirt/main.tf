5acb2a0aaf02c1fa10e1787fa0dd4dbb66e760d9
flx5 <1330854+flx5@users.noreply.github.com>
2022-03-19 18:02:43 +0100

    Remove old comment

--- libvirt/main.tf
+++ libvirt/main.tf
@@ -19,14 +19,3 @@ module "docker" {
    pool_name = libvirt_pool.stage_prod.name
    ssh_id = var.ssh_id
 }
-
-/*
-TODO Docker Provider tries to ping the docker server before anything else can run...
-     Thus containers and VMs have to be provisioned separately
-
-module "docker" {
-   source = "./modules/container"
-
-   docker_host = "ssh://core@${local.docker_ip}:22"
-   test = module.debian.docker_addresses
-}*/
