7cc3652e3380a9b83a45daf8afe72c059e8a69ba
flx5 <1330854+flx5@users.noreply.github.com>
2022-04-24 09:43:35 +0200

    media addons

--- infrastructure/docker/vm-media/main.tf
+++ infrastructure/docker/vm-media/main.tf
@@ -20,14 +20,18 @@ module "jellyfin" {
    traefik_network = docker_network.traefik_intern.name
 }
 
+module "addons" {
+   source = "git::ssh://git@github.com/flx5/homelab-addons.git//media"
+   traefik_network = docker_network.traefik_intern.name
+}
 
 module "traefik" {
    source = "../containers/traefik"
    internal_network_name = docker_network.traefik_intern.name
    wan_network_name = docker_network.lan.name
-   configurations = {
+   configurations = merge({
       jellyfin = module.jellyfin.traefik_config,
       tvheadend = module.tvheadend.traefik_config
-   }
+   }, module.addons.traefik_config)
    additional_entrypoints = {}
 }
