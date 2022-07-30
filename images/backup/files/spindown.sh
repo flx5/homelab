921fce1aa88efb85a41f2836147180c3b422199b
flx5 <1330854+flx5@users.noreply.github.com>
2022-07-30 12:31:02 +0200

    Prevent too early spindown

--- images/backup/files/spindown.sh
+++ images/backup/files/spindown.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+rm /tmp/ssh_session_open
+
+sleep 5m
+
+# Session was reopened (for example by a borg prune after borg create), so do not stop disks
+if test -f /tmp/ssh_session_open; then
+   exit 0
+fi
+
+
 /usr/bin/umount /home/nasbackup/repository
 /usr/sbin/vgchange -a n hdd
 /usr/sbin/cryptdisks_stop md0_crypt
--- images/backup/files/spinup.sh
+++ images/backup/files/spinup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo 'OPEN' > /tmp/ssh_session_open
+
 /usr/sbin/mdadm --assemble /dev/md0
 /usr/sbin/cryptdisks_start md0_crypt
 /usr/sbin/vgchange -a y hdd
