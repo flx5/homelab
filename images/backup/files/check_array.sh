12f595335b9f1a088edc8055220b957495b394a5
flx5 <1330854+flx5@users.noreply.github.com>
2022-08-21 12:19:03 +0200

    Schedule mdadm check

---
+++ images/backup/files/check_array.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# TODO Remove debug output
+
+echo "Spinning disks up for raid check"
+
+/usr/sbin/spinup.sh
+
+echo "Schedule raid check"
+/usr/share/mdadm/checkarray --cron --all --idle --quiet
+
+echo "Wait for raid check to start"
+
+while [[ $(< /sys/block/md0/md/sync_action) != "check" ]]; then
+  sleep 10s
+done
+
+echo "Wait for raid check to finish"
+
+while [[ $(< /sys/block/md0/md/sync_action) != "idle" ]]; then
+  sleep 5m
+done
+
+echo "Scheduling spindown"
+
+/usr/sbin/spindown.sh
+
+echo "Spindown result:"
+
+cat /proc/mdstat
--- images/backup/files/spindown.sh
+++ images/backup/files/spindown.sh
@@ -5,8 +5,14 @@ rm /tmp/ssh_session_open || true
 
 sleep 5m
 
+if [[ $(< /sys/block/md0/md/sync_action) != "idle" ]]; then
+  echo "Raid check active. Not stopping raid."
+  exit 0
+fi
+
 # Session was reopened (for example by a borg prune after borg create), so do not stop disks
 if test -f /tmp/ssh_session_open; then
+   echo "Session reopened. Not stopping."
    exit 0
 fi
 
@@ -14,6 +20,7 @@ fi
 /usr/bin/umount /home/nasbackup/repository
 /usr/sbin/vgchange -a n hdd
 /usr/sbin/cryptdisks_stop md0_crypt
+
 /usr/sbin/mdadm --stop /dev/md0
 
 /usr/bin/sync
