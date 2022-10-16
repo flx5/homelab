a1b7323612c4499772e5ed516870d9767ff5ff43
flx5 <1330854+flx5@users.noreply.github.com>
2022-10-16 22:02:13 +0200

    Revert authentik for media

--- infrastructure/docker/media/media.tf
+++ infrastructure/docker/media/media.tf
@@ -35,7 +35,7 @@ module "jellyfin" {
 }
 
 module "addons" {
-   source = "git::ssh://git@github.com/flx5/homelab-addons.git//media?ref=65b429a3"
+   source = "git::ssh://git@github.com/flx5/homelab-addons.git//media?ref=4f787c4"
    traefik_network = docker_network.traefik_intern.name
    base_domain = var.base_domain
 }
