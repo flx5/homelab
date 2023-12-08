8fd9d952a7d11b6ad154dcd19fb35dbbe09c03d5
flx5 <1330854+flx5@users.noreply.github.com>
2023-12-08 20:58:46 +0100

    fix traefik

--- infrastructure/docker/internal/internal.tf
+++ infrastructure/docker/internal/internal.tf
@@ -67,7 +67,7 @@ module "archivebox" {
 }
 
 module "addons" {
-  source = "git::ssh://git@github.com/flx5/homelab-addons.git//internal?ref=8518208"
+  source = "git::ssh://git@github.com/flx5/homelab-addons.git//internal?ref=36d89026"
   traefik_network = docker_network.traefik_intern.name
   base_domain = var.base_domain
 }
